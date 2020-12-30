2d171a3d6de22a51abd268239490871b943b2470
cryi <cryi@tutanota.com>
2020-12-30 20:43:14 +0100

    moved utils to separate project

--- src/lutil.h
+++ src/lutil.h
@@ -3,10 +3,8 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-char *joinpath(const char *pth1, const char *pth2);
 int push_error(lua_State *L, const char *info);
 int push_result(lua_State *L, int res, const char *info);
-FILE *check_file(lua_State *L, int idx, const char *funcname);
 
 #ifdef _WIN32
 #include <ctype.h>
