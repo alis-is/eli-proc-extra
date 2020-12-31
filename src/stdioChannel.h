201c179862cba03542243f56ef19027b9b5cc2f4
cryi <cryi@tutanota.com>
2020-12-31 22:19:39 +0100

    stdioChannel.h and lstream.h

---
+++ src/stdioChannel.h
@@ -0,0 +1,24 @@
+#ifndef STDIO_CHANNEL_H_
+#define STDIO_CHANNEL_H_
+
+#include "stream.h"
+
+#define STDIO_STDIN 0
+#define STDIO_STDOUT 1
+#define STDIO_STDERR 2
+
+typedef enum stdioChannelKind {
+    STDIO_CHANNEL_INHERIT_KIND,
+    STDIO_CHANNEL_STREAM_KIND,
+    STDIO_CHANNEL_EXTERNAL_STREAM_KIND,
+    STDIO_CHANNEL_EXTERNAL_FILE_KIND,
+    STDIO_CHANNEL_IGNORE_KIND
+} stdioChannelKind;
+
+typedef struct stdioChannel {
+    stdioChannelKind kind;
+    ELI_STREAM *stream;
+    int fdToClose;
+    luaL_Stream *file;
+} stdioChannel;
+#endif
