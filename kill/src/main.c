0d44e8dbf23e335af9dd9e2ee8e97e8b68d632d0
v <cryi@tutanota.com>
2026-01-07 19:27:56 +0100

    kill.exe improvements - wait a bit after sending the signal - do not kill itself

--- kill/src/main.c
+++ kill/src/main.c
@@ -5,15 +5,32 @@ main(int argc, char* argv[]) {
     if (argc < 2) {
         return 1;
     }
-    int success = 1;
+
+    // protect itself
+    if (!SetConsoleCtrlHandler(NULL, TRUE)) {
+        return 2;
+    }
 
     DWORD ctrlSignal = (DWORD)_atoi64(argv[argc - 1]);
+
+    int signalSent = 0;
+
     for (int i = 0; i < argc - 1; i++) {
         DWORD pid = (DWORD)_atoi64(argv[i]);
+
+        // Always detach before trying to attach (clean slate)
         FreeConsole();
-        if (!AttachConsole(pid) || !GenerateConsoleCtrlEvent(ctrlSignal, 0)) {
-            success = 0;
+
+        if (AttachConsole(pid)) {
+            // We found a live process in the group!
+            // Send the signal to the entire console group (0)
+            if (GenerateConsoleCtrlEvent(ctrlSignal, 0)) {
+                signalSent = 1;
+                Sleep(50);
+
+                break; // Job done, exit loop.
+            }
         }
     }
-    return success == 1 ? 0 : 1;
+    return signalSent ? 0 : 1;
 }
